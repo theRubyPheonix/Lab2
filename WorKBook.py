f724c7960a167625f434b1ad823b5da46104d812
Rabin Sapkota <123710399+theRubyPheonix@users.noreply.github.com>
2023-02-24 03:49:21 +0000

    Update WorKBook.py

--- WorKBook.py
+++ WorKBook.py
@@ -1,1 +1,13 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"mount_file_id":"1-SQrzXHyj_cmBFpQHJn-Qz3oqWqnRzmq","authorship_tag":"ABX9TyO7/F7rN7wXNl+ByrURqo2X"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":17,"metadata":{"id":"ISI5zKZu8hY1","executionInfo":{"status":"ok","timestamp":1677206304569,"user_tz":0,"elapsed":8,"user":{"displayName":"Rabin Sapkota","userId":"12431360272788544407"}}},"outputs":[],"source":["import os"]},{"cell_type":"code","source":["# changing the current directory location \n","os.chdir('/content/drive/MyDrive/Colab Notebooks/Lab2/')"],"metadata":{"id":"mQGvJ5jS9d0b"},"execution_count":null,"outputs":[]},{"cell_type":"code","source":["os.getcwd()"],"metadata":{"colab":{"base_uri":"https://localhost:8080/","height":35},"id":"Tdk5eMgQ9_wv","executionInfo":{"status":"ok","timestamp":1677206310182,"user_tz":0,"elapsed":8,"user":{"displayName":"Rabin Sapkota","userId":"12431360272788544407"}},"outputId":"e5a0e3f6-3ee2-411e-9ad6-a5a96360d3af"},"execution_count":18,"outputs":[{"output_type":"execute_result","data":{"text/plain":["'/content/drive/My Drive/Colab Notebooks/Lab2'"],"application/vnd.google.colaboratory.intrinsic+json":{"type":"string"}},"metadata":{},"execution_count":18}]},{"cell_type":"code","source":["python "],"metadata":{"id":"taaEw1nvNp5o","executionInfo":{"status":"error","timestamp":1677206311827,"user_tz":0,"elapsed":218,"user":{"displayName":"Rabin Sapkota","userId":"12431360272788544407"}},"outputId":"297998c0-5411-4616-b003-25a934d46996","colab":{"base_uri":"https://localhost:8080/","height":164}},"execution_count":19,"outputs":[{"output_type":"error","ename":"NameError","evalue":"ignored","traceback":["\u001b[0;31m---------------------------------------------------------------------------\u001b[0m","\u001b[0;31mNameError\u001b[0m                                 Traceback (most recent call last)","\u001b[0;32m<ipython-input-19-e150d9f71f83>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m\u001b[0m\n\u001b[0;32m----> 1\u001b[0;31m \u001b[0mpython\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m","\u001b[0;31mNameError\u001b[0m: name 'python' is not defined"]}]},{"cell_type":"code","source":[],"metadata":{"id":"5KvRwzQiSVES"},"execution_count":null,"outputs":[]}]}
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+    def mapper(self,_,line):
+        yield 'chars',len(line)
+        yield 'words',len(line.split())
+        yield 'lines',1
+
+    def reducer(self,key_values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
